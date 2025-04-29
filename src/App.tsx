f165ff5533f53403c5b8b3d4a145fa3aeabcae56
Yuta Hoshina <yuta.h.8888@gmail.com>
2025-04-29 22:56:41 +0900

    ルーティングの設定

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Report from './pages/Report';
 import NoMatch from './pages/NoMatch';
+import AppLayout from './components/layout/AppLayout';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/report" element={<Report />}/>
-        <Route path="*" element={<NoMatch />}/>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />}/>
+          <Route path="/report" element={<Report />}/>
+          <Route path="*" element={<NoMatch />}/>
+        </Route>
       </Routes>
     </Router>
   );
