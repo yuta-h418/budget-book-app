33b6854e1fc5db174a56d580e19f306211e47417
Yuta Hoshina <yuta.h.8888@gmail.com>
2025-04-29 16:09:53 +0900

    Home,Report,NotMatch各画面コンポーネントを作成

---
+++ src/pages/NoMatch.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoMatch = () => {
+  return (
+    <div>このページは存在しません。</div>
+  )
+}
+
+export default NoMatch
---
+++ src/pages/Report.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Report = () => {
+  return (
+    <div>Report</div>
+  )
+}
+
+export default Report
