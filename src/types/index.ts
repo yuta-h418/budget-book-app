255a029737a50f617398087d22a44de7c7e4bf46
Yuta Hoshina <yuta.h.8888@gmail.com>
2025-05-05 17:35:10 +0900

    DB定義を設定

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type TransactionType = "income" | "expense";
+export type IncomeCategory = "給与" | "副収入" | "お小遣い";
+export type ExpenseCategory = "食費" | "日用品" | "住居費" | "交際費" | "娯楽" | "交通費";
+
+export interface Transaction {
+    id: string,
+    date: string,
+    amount: number,
+    content: string,
+    type: TransactionType,
+    category: IncomeCategory | ExpenseCategory,
+}
