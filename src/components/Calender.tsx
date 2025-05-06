21759ceb6ae27ce2c35efabd9c55a390023f9bcc
Yuta Hoshina <yuta.h.8888@gmail.com>
2025-05-06 18:10:56 +0900

    Home画面にカレンダーを表示

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,8 +1,41 @@
+import FullCalendar from '@fullcalendar/react'
 import React from 'react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import jaLocale from '@fullcalendar/core/locales/ja'
+import "../utils/calendar.css"
+import { EventContentArg } from '@fullcalendar/core'
 
 const Calender = () => {
+  
+  const events = [
+    { title: 'Meeting', start: new Date() },
+    { title: 'money', start: "2025-05-21", income: 300, expense: 200, balance: 100},
+  ]
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    return (
+      <div>
+        <div className='money' id="event-income">
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className='money' id="event-expense">
+          {eventInfo.event.extendedProps.expense}
+        </div>
+        <div className='money' id="event-balance">
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div>Calender</div>
+    <FullCalendar 
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView='dayGridMonth'
+      events={events}
+      eventContent={renderEventContent}
+    />
   )
 }
 
