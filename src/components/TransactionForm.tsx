cc11fbfc16c97f5917fc38bfdb62391da0975604
Yuta Hoshina <yuta.h.8888@gmail.com>
2025-05-25 21:47:03 +0900

    formの支出/収入の切り替え処理追加

--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -10,6 +10,8 @@ interface TransactionFormProps {
   currentDay: string;
 }
 
+type IncomeExpense = "income" | "expense";
+
 const TransactionForm = ({ 
   onCloseForm,
   isEntryDrawerOpen,
@@ -17,7 +19,7 @@ const TransactionForm = ({
 }: TransactionFormProps) => {
   const formWidth = 320;
 
-  const { control } = useForm({
+  const { control, setValue } = useForm({
     defaultValues: {
       type: "expense",
       date: currentDay,
@@ -26,6 +28,10 @@ const TransactionForm = ({
       content: "",
     }
   });
+
+  const incomeExpenseToggle = (type: IncomeExpense) => {
+    setValue("type", type);
+  };
 
   return (
     <Box
@@ -71,14 +77,30 @@ const TransactionForm = ({
           <Controller
             name="type"
             control={control}
-            render={({ field }) => (
-              <ButtonGroup fullWidth>
-                <Button variant={"contained"} color="error">
-                  支出
-                </Button>
-                <Button>収入</Button>
-              </ButtonGroup>
-            )}
+            render={({ field }) => {
+              return (
+                <ButtonGroup fullWidth>
+                  <Button 
+                    variant={
+                      field.value === "expense" ? "contained" : "outlined"
+                    } 
+                    color="error" 
+                    onClick={() => incomeExpenseToggle("expense")}
+                  >
+                    支出
+                  </Button>
+                  <Button 
+                    onClick={() => incomeExpenseToggle("income")}
+                    color={"primary"}
+                    variant={
+                      field.value === "income" ? "contained" : "outlined"
+                    }
+                  >
+                    収入
+                  </Button>
+                </ButtonGroup>
+              )
+            }}
           />
 
           {/* 日付 */}
