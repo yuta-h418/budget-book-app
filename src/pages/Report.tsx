47f87b6fbae9dd9dcf33e450f434565575745f1a
Yuta Hoshina <yuta.h.8888@gmail.com>
2025-06-07 00:37:53 +0900

    report画面のUI構築

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,8 +1,25 @@
+import { Grid, Paper } from '@mui/material'
 import React from 'react'
 
 export const Report = () => {
+  const commonPaperStyle = {
+    height: { xs: "auto", md: "400px"},
+  }
   return (
-    <div>Report</div>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        日付
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Paper sx={commonPaperStyle}>カテゴリグラフ</Paper>
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Paper sx={commonPaperStyle}>棒グラフ</Paper>
+      </Grid>
+      <Grid item xs={12}>
+        テーブル
+      </Grid>
+    </Grid>
   )
 }
 
