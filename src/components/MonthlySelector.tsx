ea4a52708b73e325a8ce4f974c49b3881cb950a5
Yuta Hoshina <yuta.h.8888@gmail.com>
2025-06-08 17:42:04 +0900

    DatePickerを修正

--- src/components/MonthlySelector.tsx
+++ src/components/MonthlySelector.tsx
@@ -1,19 +1,34 @@
 import { Box, Button } from '@mui/material'
 import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { ja } from "date-fns/locale"
 import React from 'react'
 
 const MonthlySelector = () => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <LocalizationProvider 
+        dateAdapter={AdapterDateFns} 
+        adapterLocale={ja}
+        dateFormats={{ monthAndYear: "yyyy年 MM月" }}
+    >
         <Box
             sx={{ display: "flex", justifyContent: "center", alignItems: "center"}}
         >
             <Button color={"error"} variant={"contained"}>
                 先月
             </Button>
-            <DatePicker sx={{ mx: 2 }} />
+            <DatePicker
+                label="年月を選択"
+                sx={{ mx: 2, background: "white" }}
+                views={["year", "month"]}
+                format="yyyy年 MM月"
+                slotProps={{
+                    toolbar: {
+                        toolbarFormat: "yyyy年 MM月",
+                    },
+                }}
+            />
             <Button color={"primary"} variant={"contained"}>
                 次月
             </Button>
