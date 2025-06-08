7685d04403ac2732a4a083c5eec91b70d5002223
Yuta Hoshina <yuta.h.8888@gmail.com>
2025-06-08 17:30:52 +0900

    日付の選択エリアを実装

--- src/components/MonthlySelector.tsx
+++ src/components/MonthlySelector.tsx
@@ -1,8 +1,24 @@
+import { Box, Button } from '@mui/material'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import React from 'react'
 
 const MonthlySelector = () => {
   return (
-    <div>MonthlySelector</div>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Box
+            sx={{ display: "flex", justifyContent: "center", alignItems: "center"}}
+        >
+            <Button color={"error"} variant={"contained"}>
+                先月
+            </Button>
+            <DatePicker sx={{ mx: 2 }} />
+            <Button color={"primary"} variant={"contained"}>
+                次月
+            </Button>
+        </Box>
+    </LocalizationProvider>
   )
 }
 
